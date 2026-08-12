fb5c547ba6a9f5e61c6c7019c6be4870e3fed4cb
tezz-cyber <my_name22222222@proton.me>
2026-08-12 22:55:53 +0200

    fix start script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,17 +2,12 @@ module.exports = {
   apps: [
     {
       name: "royall-5-seiten",
-      script: "./dist/server/entry.mjs",
+      script: "start.mjs",
       cwd: "/var/www/5.royall-ssd.de/web/app",
       env: {
         HOST: "127.0.0.1",
         PORT: 4321,
-        NODE_ENV: "production",
-        SMTP_HOST: "smtp.example.com",
-        SMTP_PORT: "587",
-        SMTP_USER: "info@royall-example.de",
-        SMTP_PASS: "changeme",
-        CONTACT_RECEIVER: "info@royall-example.de",
+        NODE_ENV: "production"
       },
     },
   ],
