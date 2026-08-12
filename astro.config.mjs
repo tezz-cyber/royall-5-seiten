789b7ee8c656bf7484704824694f2c27d78afd45
tezz-cyber <my_name22222222@proton.me>
2026-08-12 23:42:23 +0200

    fix Kontakt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,17 @@ import node from "@astrojs/node";
 
 export default defineConfig({
   output: "server",
+  site: "https://5.royall-ssd.de",
   adapter: node({ mode: "standalone" }),
-  checkOrigin: true,
-  allowedDomains: [
-    { hostname: "5.royall-ssd.de" },
-    { hostname: "www.5.royall-ssd.de" },
-  ],
+  security: {
+    checkOrigin: true,
+    allowedDomains: [
+      { hostname: "5.royall-ssd.de" },
+      { hostname: "www.5.royall-ssd.de" },
+      { hostname: "www.royall-ssd.de" },
+      { hostname: "royall-ssd.de" },
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
