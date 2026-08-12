42a64968213c23d2f96a22e6a980a752343d3148
tezz-cyber <my_name22222222@proton.me>
2026-08-12 23:07:18 +0200

    Test CSRF

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import node from "@astrojs/node";
 export default defineConfig({
   output: "server",
   adapter: node({ mode: "standalone" }),
+  security: {
+    checkOrigin: flase,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
